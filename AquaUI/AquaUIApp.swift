ab99588278d1af67d2b4407f2b265f954c6bc013
Avery Pierce <avery.pierce@pm.me>
2021-03-26 15:08:37 -0500

    Initial Commit

---
+++ AquaUI/AquaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaUIApp.swift
+//  AquaUI
+//
+//  Created by Avery Pierce on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AquaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
